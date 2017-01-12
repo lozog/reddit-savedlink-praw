76b89ab195e41f82c82b5d0be5ace75e1eecc00f
Liam Ozog <liam@chalk.com>
2017-01-11 21:45:08 -0500

    fix: wrapped login in try-except. feat: added .gitignore. refactor: moved user,pw variables to separate module

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import praw
+import prawcore
 from pprint import pprint
+import sys
+from user import user,pw
+
 DEBUG_MESSAGES = True
 
 def debug_print( msg ) :
     if DEBUG_MESSAGES:
         print msg
 
-print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
 debug_print("Starting...")
 
-user = ''
-pw   = ''
-
-reddit = praw.Reddit(client_id='rsHOJ-vVYdLZuw',
+try:
+    reddit = praw.Reddit(client_id='rsHOJ-vVYdLZuw',
                      client_secret='BUa8NPGly-juAhsiTn3BYgZJ8jw',
                      password=pw,
                      user_agent='testscript by /u/tom---swift',
                      username=user)
+    print(reddit.user.me())
+except prawcore.exceptions.Forbidden:
+    print "Error logging in."
+    sys.exit(0)
+except:
+    print "unrecognized error"
+    sys.exit(0)
 
-print(reddit.user.me())
+
 # 'connected as ' +
 redditor = praw.models.Redditor(reddit,user)
 print(redditor)
