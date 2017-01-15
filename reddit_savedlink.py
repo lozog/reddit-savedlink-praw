0bdb7211e73ccc225cda43a5f3208bb6e0114d79
Liam Ozog <liam@chalk.com>
2017-01-15 02:20:58 -0500

    feat: use after to grab multiple slices of data from reddit. feat: rate limiting on api requests. docs: add an example of a savedlink json response

--- reddit_savedlink.py
+++ reddit_savedlink.py
@@ -1,6 +1,7 @@
 import praw
 import prawcore
 import sys
+from time import time
 import json
 from pprint import pprint
 from user import user,pw
@@ -11,22 +12,7 @@ def debug_print( msg ) :
     if DEBUG_MESSAGES:
         print msg
 
-# def connect():
-
-def getSavedLinks( cmd ):
-    print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-    print("Starting...")
-
-    # cmd = "list"
-    #
-    # argc = len(sys.argv)
-    # if argc == 2:
-    #     cmd = sys.argv[1]
-    # elif argc > 2:
-    #     print("usage: python main.py <cmd>")
-    #     sys.exit(0)
-
-    # reddit = connect()
+def connect():
     print("Connecting to Reddit...")
     try:
         reddit = praw.Reddit(client_id='rsHOJ-vVYdLZuw',
@@ -41,26 +27,75 @@ def getSavedLinks( cmd ):
     except:
         print "unrecognized error"
         sys.exit(0)
+    return reddit
+
+def getSavedLinksPRAW(redditor, limit, after, count=25):
+    print("Getting saved links")
+    try:
+        if after != None:
+            print after
+            savedLinks = redditor.saved(limit=limit,params={'after':after,'count':count})
+        else:
+            savedLinks = redditor.saved(limit=limit)
+    except prawcore.exceptions.Forbidden:
+        print "Error connecting."
+        sys.exit(0)
+
+    print("Got 'em.")
+    return savedLinks
+
+def getSavedLinks( cmd ):
+    print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+    print("Starting...")
+
+    reddit = connect()
 
     print("Connected as "),
     redditor = praw.models.Redditor(reddit,user)
     print(redditor)
 
-    print("Getting saved links")
-    savedLinks = redditor.saved()
-    print("Got 'em.")
+    limit = 100
+    reqTime = time()
+    print reqTime
+    savedLinks = getSavedLinksPRAW(redditor, limit, None)
 
     savedLinkOut = []
     if cmd == "list":
-        for link in savedLinks:
-            linkdata = vars(link)
-            print(linkdata["name"] + ": "),
+        i = 0
+        while True:
+            # ratelimit: check to see if enough time has elapsed
+            if ( time() - reqTime < 2 ):
+                # busy-wait until enough time has elapsed
+                continue
 
-            if linkdata["name"][:2] == "t1":
-                savedLinkOut.append(linkdata["link_title"])
-            elif linkdata["name"][:2] == "t3":
-                savedLinkOut.append(linkdata["title"])
-            print("a")
+            print time()
+
+            count = limit
+            for link in savedLinks:
+                linkdata = vars(link)
+                fullname = linkdata["name"]
+                # print(fullname + ": "),
+
+                if fullname[:2] == "t1":
+                    savedLinkOut.append(linkdata["link_title"])
+                elif fullname[:2] == "t3":
+                    savedLinkOut.append(linkdata["title"])
+                # pprint(linkdata)
+                count -= 1
+
+            print ("count: %d" % count)
+
+            if ( i == 5 ):
+                break
+
+            if ( count > limit ):
+                # if we didn't reach the limit, we're at the end of the content
+                break
+
+            print "retrieving more..."
+            savedLinks = getSavedLinksPRAW(redditor, limit, fullname, count)
+            i += 1
+
     elif cmd == "dev":
         linkdata = None
         for link in savedLinks:
